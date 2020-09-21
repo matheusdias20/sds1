4ba089ab91ddc74950c37c864be3b2334254725f
Matheus Vieira <mvdias20@gmail.com>
2020-09-21 16:47:14 -0300

    Finalizando app React Native

--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesome5 as Icon } from '@expo/vector-icons'
+import { FontAwesome5 as Icon } from '@expo/vector-icons';
 import { Text, StyleSheet, View, Image, Alert } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import Header from '../../components/Header';
